26342e827778a3a81fad6b3b714d82261542525b
Rudrabhatt0607 <r.m.bhatt8909@gmail.com>
2026-03-30 22:49:13 +0530

    fundamental  program

---
+++ fundamental booster.py
@@ -0,0 +1,19 @@
+name=input("enter your name : ")
+age=int(input("enter your age :"))
+hight=float(input("enter your hight : "))
+
+running_year=2026
+your_birth_year=running_year-age
+
+print(your_birth_year)
+
+print("name",name)
+print("age",age)
+print("hight",hight)
+
+print(float(age))
+print(int(hight))
+
+# type() id()
+print(type(name))
+print(id(age))
